1c044f5731d69c1164f881be5b46652d2e524549
Winstheru <45647455+Winstheru@users.noreply.github.com>
2019-03-30 21:20:25 +0700

    Update Login.java

--- app/src/main/java/com/example/kerjain/Login.java
+++ app/src/main/java/com/example/kerjain/Login.java
@@ -1,13 +1,29 @@
 package com.example.kerjain;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void openActivity2(View view){
+        Intent intent = new Intent(this, BottomNavigationView.class);
+        startActivity(intent);
+    }
+
+    public void daftarSebagai(View view){
+        Intent intent = new Intent(this, DaftarSebagai.class);
+        startActivity(intent);
     }
 }
