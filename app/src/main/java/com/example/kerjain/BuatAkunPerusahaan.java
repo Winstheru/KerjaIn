6e6ea50b4c1aba10904f7f37f125a75908d64b5f
Winstheru <45647455+Winstheru@users.noreply.github.com>
2019-04-21 13:09:29 +0700

    update listview list with kota, kabupaten dan kecamatan

--- app/src/main/java/com/example/kerjain/BuatAkunPerusahaan.java
+++ app/src/main/java/com/example/kerjain/BuatAkunPerusahaan.java
@@ -1,9 +1,9 @@
 package com.example.kerjain;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class BuatAkunPerusahaan extends AppCompatActivity {
 
@@ -11,10 +11,20 @@ public class BuatAkunPerusahaan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buat_akun_perusahaan);
-    }
 
-    public void submitPerusahaan(View view){
-        Intent perusahaan= new Intent(this, BottomNavigationView.class);
-        startActivity(perusahaan);
+        Spinner spinner1 = findViewById(R.id.provinsi);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.provinsi, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+        Spinner spinner2 = findViewById(R.id.kotakabupaten);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.kabupaten, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
+        Spinner spinner3 = findViewById(R.id.kecamatan);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.kecamatan, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
     }
 }
