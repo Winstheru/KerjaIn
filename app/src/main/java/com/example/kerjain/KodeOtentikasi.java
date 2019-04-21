118f9b66d3b618ef0dad8ee6e408375c0b42d64c
Winstheru <45647455+Winstheru@users.noreply.github.com>
2019-04-21 11:40:36 +0700

    Update KodeOtentikasi.java

--- app/src/main/java/com/example/kerjain/KodeOtentikasi.java
+++ app/src/main/java/com/example/kerjain/KodeOtentikasi.java
@@ -1,5 +1,6 @@
 package com.example.kerjain;
 
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,46 +12,64 @@ import java.util.Locale;
 
 public class KodeOtentikasi extends AppCompatActivity {
 
-    private static final long START_TIME = 200000;
-
+    //private static final long START_TIME = 200000;
+    private static final long START_TIME = 5000;
     private TextView mTextViewCountDown;
     private Button mButtonStart;
     private CountDownTimer mCountDownTimer;
     private boolean mTimerRunning; //utk cek apakah timer sedang berjalan
     private long mTimeLeft = START_TIME;
-    public int counter = 7;
-    Button button;
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kode_otentikasi);
 
-        textView = findViewById(R.id.timer);
-        button = findViewById(R.id.submit);
+        mTextViewCountDown = findViewById(R.id.timer);
+        mButtonStart = findViewById(R.id.submit);
 
         //to define what button will do when we click them
-        button.setOnClickListener(new View.OnClickListener() {
+        mButtonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new CountDownTimer(7000,1000){
-                    @Override
-                    public void onTick(long millisUntilFinished) {
-                        textView.setText(String.valueOf(counter));
-                        counter--;
-
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        mTimerRunning = false;
-                    }
-                }.start();
-                mTimerRunning = true;
+                if(mTimerRunning == false){
+                    startTimer();
+                }
             }
         });
+    }
 
-        
+    private void startTimer(){
+        //syntax CountDownTimer(long milisecInFuture, longCountDownInterval) dimana millisecInFuture is the time you set in millisecond when you want CountDownTimer to stop and countDownInterval is the interval time in millisecond you set after which number will increment in CountDownTimer.
+        mCountDownTimer = new CountDownTimer(mTimeLeft, 1000) { //countDownInterval itu fungsinya utk: tiap brp milisecondkah, method onTick akan dijalankan
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeft = millisUntilFinished; // utk save nilainya jd kita bs continue dari waktu yg sblmnya
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                //this code will run when time is up
+                mTimerRunning = false;
+                waktuHabis();
+            }
+        }.start();
+
+        mTimerRunning = true;
+    }
+
+    private void updateCountDownText(){
+        int minutes = (int) (mTimeLeft / 1000) / 60; //di-cast ke int karna type_defaultnya long, dibagi seribu biar dari miliseconds jd seconds, dibagi 60 biar jd menit
+        int seconds = (int) (mTimeLeft / 1000) % 60; //modulus is to return what is left after calculating the minutes
+
+        //format this value to be like clock
+        String timeLeftFormat = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        mTextViewCountDown.setText(timeLeftFormat);
+    }
+
+    public void waktuHabis(){
+        Intent intent = new Intent(this, DaftarSebagai.class);
+        startActivity(intent);
     }
 }
