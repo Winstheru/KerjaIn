fa5364d3ea05c23688f9e29bcc076e4f912a1d5c
Winstheru <45647455+Winstheru@users.noreply.github.com>
2019-04-22 22:55:31 +0700

    seperti saran ko ferry

--- app/src/main/java/com/example/kerjain/BuatAkunPerusahaan.java
+++ app/src/main/java/com/example/kerjain/BuatAkunPerusahaan.java
@@ -1,296 +1,69 @@
-<?xml version="1.0" encoding="utf-8"?>
-<ScrollView xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="fill_parent"
-    android:layout_height="fill_parent"
-    tools:context=".BuatAkunPerusahaan"
-    >
+package com.example.kerjain;
 
-    <LinearLayout
-        android:layout_width="match_parent"
-        android:layout_height="match_parent"
-        android:layout_marginLeft="32dp"
-        android:layout_marginRight="32dp"
-        android:orientation="vertical"
-        >
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
-        <RelativeLayout
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content">
-
-            <TextView
-                android:id="@+id/buatakun"
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:layout_marginTop="16dp"
-                android:textColor="#003B60"
-                android:text="Buat Akun"
-                android:textSize="32sp"/>
-
-            <View
-                android:layout_width="100dp"
-                android:layout_height="5dp"
-                android:layout_below="@id/buatakun"
-                android:background="#34ABA1"/>
-
-            <ImageView
-                android:layout_width="60dp"
-                android:layout_height="50dp"
-                android:layout_alignParentRight="true"
-                android:layout_marginTop="16dp"
-                android:layout_marginRight="0dp"
-                android:src="@drawable/companylogo" />
-
-        </RelativeLayout>
+public class BuatAkunPerusahaan extends AppCompatActivity {
+    private EditText namaPerusahaan, emailPerusahaan, namaPenanggungjawab, nomorKtp, nomorTelepon, kodePos, nomorNpwp, nomorSiup, alamatLengkap;
+    private Spinner provinsi, kotaKabupaten, kecamatan;
+    private String namaPer, emailPer, namaPen, ktp, mobile, npwp, siup, alamat;
+    private Button submit;
 
 
-        <EditText
-            android:id="@+id/namaperusahaan"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="24dp"
-            android:ems="8"
-            android:hint="Nama Perusahaan*"
-            android:inputType="textCapSentences|textPersonName"
-            android:singleLine="true"
-            android:textSize="20sp" />
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buat_akun_perusahaan);
 
-        <EditText
-            android:id="@+id/emailperusahaan"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:ems="8"
-            android:hint="Alamat Email Perusahaan*"
-            android:inputType="textEmailAddress"
-            android:singleLine="true"
-            android:textSize="20sp" />
-
-        <EditText
-            android:id="@+id/namapenanggungjawab"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="24dp"
-            android:ems="8"
-            android:hint="Nama Penanggung Jawab*"
-            android:inputType="textCapSentences|textPersonName"
-            android:singleLine="true"
-            android:textSize="20sp" />
+        namaPerusahaan = findViewById(R.id.namaperusahaan);
+        emailPerusahaan = findViewById(R.id.emailperusahaan);
+        namaPenanggungjawab = findViewById(R.id.namapenanggungjawab);
+        nomorKtp = findViewById(R.id.nomorktp);
+        nomorTelepon = findViewById(R.id.nomortelpon);
+        nomorNpwp = findViewById(R.id.nomornpwp);
+        nomorSiup = findViewById(R.id.nomorsiup);
+        submit = findViewById(R.id.masuk);
+        alamatLengkap = findViewById(R.id.alamatlengkap);
 
 
-        <EditText
-            android:id="@+id/nomorktp"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:ems="8"
-            android:hint="No. KTP*"
-            android:inputType="numberSigned"
-            android:singleLine="true"
-            android:maxLength="16"
-            android:textSize="20sp" />
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputData();
+                submitData();
+            }
+        });
+    }
 
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:text="Lampirkan Foto KTP Penanggung Jawab"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
+    public void inputData() {
+        mobile = nomorTelepon.getText().toString().trim();
+        namaPer = namaPerusahaan.getText().toString().trim();
+        namaPen = namaPenanggungjawab.getText().toString().trim();
+        ktp = nomorKtp.getText().toString().trim();
+        npwp = nomorNpwp.getText().toString().trim();
+        siup = nomorSiup.getText().toString().trim();
+        alamat = alamatLengkap.getText().toString().trim();
+    }
 
-        <RelativeLayout
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content">
-
-            <ImageView
-                android:layout_width="66dp"
-                android:layout_height="63dp"
-                android:layout_marginTop="8dp"
-                android:background="@drawable/roundedborder" />
-
-            <ImageView
-                android:id="@+id/lampiranfotoktppenanggungjawab"
-                android:layout_width="54dp"
-                android:layout_height="52dp"
-                android:layout_marginLeft="8dp"
-                android:layout_marginTop="15dp"
-                android:layout_marginRight="10dp"
-                android:src="@drawable/insertimage" />
-        </RelativeLayout>
-
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:text="No. Telepon*"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
-
-        <RelativeLayout
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content">
-
-            <TextView
-                android:id="@+id/kodenomortelp"
-                android:layout_width="wrap_content"
-                android:layout_height="match_parent"
-                android:text="+62"
-                android:textSize="30dp" />
-
-            <EditText
-                android:id="@+id/nomortelpon"
-                android:layout_width="match_parent"
-                android:layout_height="wrap_content"
-                android:layout_alignBottom="@+id/kodenomortelp"
-                android:layout_marginBottom="-11dp"
-                android:layout_toRightOf="@+id/kodenomortelp"
-                android:ems="10"
-                android:inputType="phone"
-                android:maxLength="12"
-                android:singleLine="true"
-                android:textSize="30sp" />
-
-        </RelativeLayout>
-
-
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:text="Pilih Provinsi*"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
-
-        <Spinner
-            android:id="@+id/provinsi"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="8dp"
-            android:background="@android:drawable/btn_dropdown"
-            android:dropDownWidth="wrap_content"
-            android:spinnerMode="dropdown" />
-
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="8dp"
-            android:text="Pilih Kota / Kabupaten*"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
-
-        <Spinner
-            android:id="@+id/kotakabupaten"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="8dp"
-            android:background="@android:drawable/btn_dropdown"
-            android:dropDownWidth="wrap_content"
-            android:spinnerMode="dropdown" />
-
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="8dp"
-            android:text="Pilih Kecamatan*"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
-
-        <Spinner
-            android:id="@+id/kecamatan"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="8dp"
-            android:background="@android:drawable/btn_dropdown"
-            android:dropDownWidth="wrap_content"
-            android:spinnerMode="dropdown" />
-
-        <EditText
-            android:id="@+id/kodepos"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="24dp"
-            android:ems="8"
-            android:hint="Kode Pos"
-            android:inputType="numberSigned"
-            android:singleLine="true"
-            android:maxLength="5"
-            android:textSize="20sp" />
-
-        <EditText
-            android:id="@+id/alamatlengkap"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="24dp"
-            android:ems="8"
-            android:hint="Alamat Lengkap"
-            android:inputType="textMultiLine|textPostalAddress"
-            android:textSize="20sp" />
-
-        <EditText
-            android:id="@+id/nomornpwp"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="24dp"
-            android:ems="8"
-            android:hint="No. NPWP*"
-            android:inputType="numberSigned"
-            android:singleLine="true"
-            android:maxLength="16"
-            android:textSize="20sp" />
-
-        <EditText
-            android:id="@+id/nomorsiup"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="24dp"
-            android:ems="8"
-            android:hint="No. SIUP"
-            android:inputType="numberSigned"
-            android:singleLine="true"
-            android:maxLength="16"
-            android:textSize="20sp" />
-
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:text="Lampirkan Foto Gedung Perusahaan"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
-
-        <RelativeLayout
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content">
-
-            <ImageView
-                android:layout_width="66dp"
-                android:layout_height="63dp"
-                android:layout_marginTop="8dp"
-                android:background="@drawable/roundedborder" />
-
-            <ImageView
-                android:id="@+id/lampiranfotogedungperusahaan"
-                android:layout_width="54dp"
-                android:layout_height="52dp"
-                android:layout_marginLeft="8dp"
-                android:layout_marginTop="15dp"
-                android:layout_marginRight="10dp"
-                android:src="@drawable/insertimage" />
-        </RelativeLayout>
-
-
-        <Button
-            android:id="@+id/masuk"
-            android:layout_width="250dp"
-            android:layout_height="60dp"
-            android:layout_gravity="center_horizontal"
-            android:layout_marginTop="16dp"
-            android:layout_marginBottom="8dp"
-            android:background="@drawable/roundedbuttonblue"
-            android:text="Submit"
-            android:textColor="#003B60"
-            />
-
-    </LinearLayout>
-</ScrollView>
+    public void submitData(){
+        Intent intent = new Intent(BuatAkunPerusahaan.this, KodeOtentikasi.class);
+        intent.putExtra("mobile", mobile);
+        intent.putExtra("namaPer",namaPer);
+        intent.putExtra("namaPen",namaPen);
+        intent.putExtra("emailPer",emailPer);
+        intent.putExtra("alamat", alamat);
+        intent.putExtra("ktp", ktp);
+        intent.putExtra("npwp", npwp);
+        intent.putExtra("siup", siup);
+        //intent.putExtra("email", email);
+        intent.putExtra("func", "registerPr");
+        startActivity(intent);
+    }
+}
