c80cccecd91d02423adf0c30a55aea5ecf3582bd
Winstheru <45647455+Winstheru@users.noreply.github.com>
2019-04-22 22:55:58 +0700

    seperti kata ko ferry

--- app/src/main/java/com/example/kerjain/BuatAkunPekerja.java
+++ app/src/main/java/com/example/kerjain/BuatAkunPekerja.java
@@ -1,285 +1,89 @@
-<?xml version="1.0" encoding="utf-8"?>
-<ScrollView xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    >
+package com.example.kerjain;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 
-    <LinearLayout
-        android:layout_width="match_parent"
-        android:layout_height="match_parent"
-        android:layout_marginLeft="32dp"
-        android:layout_marginRight="32dp"
-        android:orientation="vertical"
-        tools:context=".BuatAkunPekerja"
-        >
+public class BuatAkunPekerja extends AppCompatActivity {
+    private EditText namaPekerja, emailPekerja, noKtp, nomorTelepon, alamatLengkap;
+    private RadioButton radioPria, radioWanita;
+    private RadioGroup rgGender;
+    private Spinner provinsi, kotaKabupaten, kecamatan;
+    private Button register;
+    private String gender, mobile, name, ktp, email, alamat, provinsi1, kotaKabupaten1, kecamatan1, func;
 
-        <RelativeLayout
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content">
+    @Override
 
-            <TextView
-                android:id="@+id/buatakun"
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:layout_marginTop="16dp"
-                android:text="Buat Akun"
-                android:textSize="32sp"/>
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buat_akun_pekerja);
 
-            <View
-                android:layout_width="100dp"
-                android:layout_height="5dp"
-                android:layout_below="@id/buatakun"
-                android:background="#34ABA1"/>
-
-            <ImageView
-                android:layout_marginTop="16dp"
-                android:layout_width="60dp"
-                android:layout_height="50dp"
-                android:src="@drawable/workerlogo"
-                android:layout_alignParentRight="true"/>
-
-        </RelativeLayout>
+        namaPekerja = findViewById(R.id.namapekerja);
+        noKtp = findViewById(R.id.nomorktp);
+        emailPekerja = findViewById(R.id.emailpekerja);
+        nomorTelepon = findViewById(R.id.nomortelpon);
+        alamatLengkap = findViewById(R.id.alamatlengkap);
+        register = findViewById(R.id.register);
+        rgGender = findViewById(R.id.rgGender);
+        radioPria = findViewById(R.id.radio_pria);
+        radioWanita = findViewById(R.id.radio_wanita);
 
 
-        <EditText
-            android:id="@+id/namapekerja"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="24dp"
-            android:ems="8"
-            android:hint="Nama Lengkap*"
-            android:inputType="textCapSentences|textPersonName"
-            android:singleLine="true"
-            android:textSize="20sp" />
 
-        <EditText
-            android:id="@+id/emailpekerja"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:ems="8"
-            android:hint="Alamat Email*"
-            android:inputType="textEmailAddress"
-            android:singleLine="true"
-            android:textSize="20sp" />
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputData();
+                submitData();
+            }
+        });
+    }
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
 
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:text="Gender"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_pria:
+                if (checked)
+                    gender="Pria";
+                break;
+            case R.id.radio_wanita:
+                if (checked)
+                    gender="Wanita";
+                break;
+        }
+    }
+    public void inputData(){
+        mobile = nomorTelepon.getText().toString().trim();
+        name = namaPekerja.getText().toString().trim();
+        ktp = noKtp.getText().toString().trim();
+        email = emailPekerja.getText().toString().trim();
+        alamat = alamatLengkap.getText().toString().trim();
+        provinsi1 = provinsi.getSelectedItem().toString().trim();
+        kotaKabupaten1 = kotaKabupaten.getSelectedItem().toString().trim();
+        kecamatan1 = kecamatan.getSelectedItem().toString().trim();
+    }
+    public void submitData(){
+        Intent intent = new Intent(BuatAkunPekerja.this, KodeOtentikasi.class);
+        intent.putExtra("mobile", mobile);
+        intent.putExtra("name",name);
+        intent.putExtra("alamat", alamat);
+        intent.putExtra("provinsi", provinsi1);
+        intent.putExtra("kotakabupaten", kotaKabupaten1);
+        intent.putExtra("kecamatan", kecamatan1);
+        intent.putExtra("ktp", ktp);
+        intent.putExtra("email", email);
+        intent.putExtra("func", "registerPk");
+        startActivity(intent);
+    }
+}
 
-        <!--<LinearLayout-->
-        <!--android:layout_width="match_parent"-->
-        <!--android:layout_height="wrap_content"-->
-        <!--android:orientation="horizontal">-->
-
-        <RadioGroup xmlns:android="http://schemas.android.com/apk/res/android"
-            android:id="@+id/rgGender"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:orientation="horizontal">
-
-            <RadioButton android:id="@+id/radio_pria"
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:text="Pria"
-                android:onClick="onRadioButtonClicked"/>
-
-            <RadioButton android:id="@+id/radio_wanita"
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:text="Wanita"
-                android:layout_marginLeft="16dp"
-                android:onClick="onRadioButtonClicked"/>
-        </RadioGroup>
-
-
-        <EditText
-            android:id="@+id/nomorktp"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:ems="8"
-            android:hint="No. KTP*"
-            android:inputType="numberSigned"
-            android:singleLine="true"
-            android:maxLength="16"
-            android:textSize="20sp" />
-
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:text="Lampirkan Foto KTP"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
-
-        <RelativeLayout
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content">
-
-            <ImageView
-                android:layout_width="66dp"
-                android:layout_height="63dp"
-                android:layout_marginTop="8dp"
-                android:background="@drawable/roundedborder" />
-
-            <ImageView
-                android:id="@+id/lampiranfotoktppekerja"
-                android:layout_width="54dp"
-                android:layout_height="52dp"
-                android:layout_marginLeft="8dp"
-                android:layout_marginTop="15dp"
-                android:layout_marginRight="10dp"
-                android:src="@drawable/insertimage" />
-        </RelativeLayout>
-
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:text="No. Telepon*"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
-
-        <RelativeLayout
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content">
-
-            <TextView
-                android:id="@+id/kodenomortelp"
-                android:layout_width="wrap_content"
-                android:layout_height="match_parent"
-                android:text="+62"
-                android:textSize="30dp" />
-
-            <EditText
-                android:id="@+id/nomortelpon"
-                android:layout_width="match_parent"
-                android:layout_height="wrap_content"
-                android:layout_alignBottom="@+id/kodenomortelp"
-                android:layout_marginBottom="-11dp"
-                android:layout_toRightOf="@+id/kodenomortelp"
-                android:ems="10"
-                android:inputType="phone"
-                android:maxLength="12"
-                android:singleLine="true"
-                android:textSize="30sp" />
-
-        </RelativeLayout>
-
-
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="16dp"
-            android:text="Pilih Provinsi*"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
-
-        <Spinner
-            android:id="@+id/provinsi"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="8dp"
-            android:background="@android:drawable/btn_dropdown"
-            android:dropDownWidth="wrap_content"
-            android:spinnerMode="dropdown" />
-
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="8dp"
-            android:text="Pilih Kota / Kabupaten*"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
-
-        <Spinner
-            android:id="@+id/kotakabupaten"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="8dp"
-            android:background="@android:drawable/btn_dropdown"
-            android:dropDownWidth="wrap_content"
-            android:spinnerMode="dropdown" />
-
-        <TextView
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="8dp"
-            android:text="Pilih Kecamatan*"
-            android:textColor="@android:color/black"
-            android:textSize="18sp" />
-
-        <Spinner
-            android:id="@+id/kecamatan"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="8dp"
-            android:background="@android:drawable/btn_dropdown"
-            android:dropDownWidth="wrap_content"
-            android:spinnerMode="dropdown" />
-
-        <EditText
-            android:id="@+id/kodepos"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="24dp"
-            android:ems="8"
-            android:hint="Kode Pos"
-            android:inputType="numberSigned"
-            android:singleLine="true"
-            android:maxLength="5"
-            android:textSize="20sp" />
-
-        <EditText
-            android:id="@+id/alamatlengkap"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="24dp"
-            android:ems="8"
-            android:hint="Alamat Lengkap"
-            android:inputType="text|textPostalAddress"
-            android:textSize="20sp" />
-
-        <EditText
-            android:id="@+id/katasandi"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="24dp"
-            android:ems="8"
-            android:hint="Kata Sandi"
-            android:inputType="textPassword"
-            android:singleLine="true"
-            android:textSize="20sp" />
-
-        <EditText
-            android:id="@+id/ulangkatasandi"
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:layout_marginTop="24dp"
-            android:ems="8"
-            android:hint="Ulangi Kata Sandi"
-            android:inputType="textPassword"
-            android:singleLine="true"
-            android:textSize="20sp" />
-
-        <Button
-            android:id="@+id/register"
-            android:layout_width="250dp"
-            android:layout_height="60dp"
-            android:layout_gravity="center_horizontal"
-            android:layout_marginTop="16dp"
-            android:layout_marginBottom="8dp"
-            android:background="@drawable/roundedbuttonblue"
-            android:text="Submit"
-            android:textColor="#003B60" />
-
-    </LinearLayout>
-</ScrollView>
