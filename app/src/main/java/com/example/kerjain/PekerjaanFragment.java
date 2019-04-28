508b6044ffc0fe74e1979c333e342eb7abae2230
Winstheru <45647455+Winstheru@users.noreply.github.com>
2019-04-28 08:41:36 +0700

    Create PekerjaanFragment.java

---
+++ app/src/main/java/com/example/kerjain/PekerjaanFragment.java
@@ -0,0 +1,24 @@
+package com.example.kerjain;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PekerjaanFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pekerjaan,container,false);
+    }
+
+
+
+
+
+}
